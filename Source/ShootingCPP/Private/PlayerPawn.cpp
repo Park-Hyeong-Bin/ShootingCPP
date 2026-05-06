d93ac8adbd68af139f016d409bc5865da68f0251
박형빈 <cavin1999@gmail.com>
2026-05-06 18:06:50 +0900

    feat: PlayerPawn 컴포넌트 구성 - BoxComponent/StaticMeshComponent 생성자에 추가

---
+++ Source/ShootingCPP/Private/PlayerPawn.cpp
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPawn.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// 박스 콜리전 컴포넌트 생성
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("My Box Component"));
+	
+	// 생성한 박스 콜리전 컴포넌트를 최상단 컴포넌트로 설정
+	SetRootComponent(boxComp);
+	
+	// 스태틱메시 컴포넌트 생성
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My Mesh Component"));
+	
+	// 박스 콜리전 자식으로 스태틱 메시 컴포넌트 설정
+	meshComp->SetupAttachment(boxComp);
+	
+	// 박스 콜라이더 크기를 50x50x50 설정
+	FVector boxSize = FVector(50.f, 50.f, 50.f);
+	boxComp->SetBoxExtent(boxSize);
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	APlayerController* pc = GetWorld()->GetFirstPlayerController();
+	if (pc != nullptr)
+	{
+		// 플레이어 컨트롤러로부터 입력 서브시스템 정보 가져오기
+		UEnhancedInputLocalPlayerSubsystem* subsys = 
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+		if (subsys != nullptr)
+		{
+			subsys->AddMappingContext(imcPlayerInput, 0);
+		}
+	}
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	// Tick 당, 매프레임 마다 호출됨
+	// 사용자 입력 키를 이용해서
+	FVector dir = FVector(0,h,v);
+	dir.Normalize(); // 방향 벡터 길이가 1이 되도록 정규화(1로 제한)
+	// 현재위치 + 방향*속력*시간
+	FVector newLocation = GetActorLocation() + dir * moveSpeed * DeltaTime;
+	SetActorLocation(newLocation);
+}
+
+// 사용자가 키를 누를때 실행되며 h,v 변수를 재할당하는 함수
+void APlayerPawn::OnInputHorizontal(const struct FInputActionValue& value)
+{
+	h = value.Get<float>();
+}
+
+void APlayerPawn::OnInputVertical(const struct FInputActionValue& value)
+{
+	v = value.Get<float>();
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* eic = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (eic != nullptr)
+	{
+		eic->BindAction(iaHorizontal, ETriggerEvent::Triggered, this, &APlayerPawn::OnInputHorizontal);
+		eic->BindAction(iaHorizontal, ETriggerEvent::Completed, this, &APlayerPawn::OnInputHorizontal);
+		eic->BindAction(iaVertical, ETriggerEvent::Triggered, this, &APlayerPawn::OnInputVertical);
+		eic->BindAction(iaVertical, ETriggerEvent::Completed, this, &APlayerPawn::OnInputVertical);
+	}
+}
---
+++ Source/ShootingCPP/Private/PlayerPawn.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class SHOOTINGCPP_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// 박스 충돌체 컴포넌트
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* boxComp;
+	
+	// 스태틱 메시 컴포넌트
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* meshComp;
+	
+	// IMC 파일의 포인터 변수
+	UPROPERTY(EditAnywhere)
+	class UInputMappingContext* imcPlayerInput;
+	
+	// IA 파일의 포인터 변수
+	UPROPERTY(EditAnywhere)
+	class UInputAction* iaHorizontal;
+	
+	UPROPERTY(EditAnywhere)
+	class UInputAction* iaVertical;
+	
+	// 이동 속도 변수
+	UPROPERTY(EditAnywhere)
+	float moveSpeed = 500.f;
+	
+private:
+	// 상요자 키 입력값을 받을 변수
+	float h;
+	float v;
+	
+	// 입력 이벤트 발동 시 실행 될 함수
+	void OnInputHorizontal(const struct FInputActionValue& value);
+	void OnInputVertical(const struct FInputActionValue& value);
+};
