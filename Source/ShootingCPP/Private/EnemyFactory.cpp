636c999a52103d245dfcb7bcb2cb3469dc05ae01
박형빈 <cavin1999@gmail.com>
2026-05-07 15:20:36 +0900

    feat : EnemyFactory 적 생성 로직 구현 - 타이머에 다른 주기적 EnemyActor 스폰

--- Source/ShootingCPP/Private/EnemyFactory.cpp
+++ Source/ShootingCPP/Private/EnemyFactory.cpp
@@ -3,6 +3,8 @@
 
 #include "EnemyFactory.h"
 
+#include "EnemyActor.h"
+
 
 // Sets default values
 AEnemyFactory::AEnemyFactory()
@@ -22,5 +24,16 @@ void AEnemyFactory::BeginPlay()
 void AEnemyFactory::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	//현재 프레임 시간을 누적
+	currentTime += DeltaTime;
+	
+	if (currentTime >= delayTime)
+	{
+		currentTime = 0; // 누적 시간 초기화
+		AEnemyActor* spawnActor = GetWorld()->SpawnActor<AEnemyActor>(enemy,
+			GetActorLocation(),
+			GetActorRotation());
+	}
 }
 
